e7f77af593eeab62d94e94ced76dc4604904bdf2
jaclynchorton <jaclynchorton@gmail.com>
2019-12-13 14:05:18 -0800

    Commit Factorial-SqRt-GCD-DegreeAndRadians.py

---
+++ Factorial-SqRt-GCD-DegreeAndRadians.py
@@ -0,0 +1,20 @@
+#------------------From Learning the Python 3 Standard Library------------------
+
+#importing math functions
+import math
+
+#---------------------------Factorial and Square Root---------------------------
+# Factorial of 3 = 3 * 2* 1
+print(math.factorial(3))
+# Squareroot of 64
+print(math.sqrt(64))
+# GCD-Greatest Common Denominator.  Useful for reducing fractions
+# 8/52 = 2/13
+# GCD between 8 and 52 is 4, so we can divide the numerator and denominator by 4
+# to get a reduced fraction
+print(math.gcd(52, 8))
+print(math.gcd(8, 52))
+
+print(8/52)
+print(2/13)
+
